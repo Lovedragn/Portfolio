31b295f923e304c9a149b376777a2c417b3f7589
Sujith_SSS <121754425+Lovedragn@users.noreply.github.com>
2025-07-23 23:45:57 +0530

    Update Aboutme.jsx

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -78,7 +78,7 @@ const Aboutme = () => {
             ease: "power3.out",
             scrollTrigger: {
               trigger: videoRef.current,
-             start:"top 50%",
+             start:"top 40%",
               onEnter: () => {
                 if (
                   videoRef.current &&
